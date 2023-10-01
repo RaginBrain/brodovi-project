45b370a8d95933d031ec03cb3f3a492664cd0fb8
Antimon <ante@alterego.hr>
2023-10-01 15:34:51 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Material UI - CRA example with Tailwind CSS
+
+## How to use
+
+clone the repo
+
+
+
+
+Install it and run:
+
+```bash
+npm install
+npm start
+```
+
+or:
+
+<!-- #default-branch-switch -->
+
+[![Edit on StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/mui/material-ui/tree/master/examples/brodovi-project)
+
+## The idea behind the example
+
+<!-- #default-branch-switch -->
+
+This example demonstrates how you can use [Tailwind CSS](https://tailwindcss.com/) and [Create React App](https://github.com/facebookincubator/create-react-app) together with Material UI.
+It includes `@mui/material` and its peer dependencies, including [Emotion](https://emotion.sh/docs/introduction), the default style engine in Material UI v5.
+
+## What's next?
+
+<!-- #default-branch-switch -->
+I dont know why but we are on branch master....
+
+
+You now have a working example project.
+You can head back to the documentation and continue by browsing the [templates](https://mui.com/material-ui/getting-started/templates/) section.
---
+++ src/PopoverMenu.jsx
@@ -0,0 +1,45 @@
+import { Button, Menu, MenuItem } from '@mui/material';
+import React, { useState } from 'react';
+
+export default function PopoverMenu() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        Popover Menu
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose} className="py-2">
+          Small Item
+        </MenuItem>
+        <MenuItem onClick={handleClose} className="py-8">
+          Large Item
+        </MenuItem>
+      </Menu>
+    </>
+  );
+}
